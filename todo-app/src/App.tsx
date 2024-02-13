95637883c4cb8dc14ee1c43647a2b9cba29886f8
Patorok <patricklouisejacinto@gmail.com>
2024-02-13 16:26:05 +0800

    Remove commented code and unused imports in App component

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,15 +1,7 @@
 import './App.css';
 import TodoList from "./views/todo-list";
 
-// import { useState } from "react";
-// import Alert from "./components/Alert";
-// import Button from "./components/Button";
-
 function App() {
-    // const [alertVisible, setAlertVisibility] = useState(false);
-    // { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>Hello World!</Alert>}
-    // <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
-
     return (
         <>
             <div className="container-sm">
